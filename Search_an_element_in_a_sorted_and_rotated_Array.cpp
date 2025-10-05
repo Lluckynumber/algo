5973f6cd6642dc1ede70f7e56f6ed8eb700552b3
Lluckynumber <115993719+Lluckynumber@users.noreply.github.com>
2025-10-05 10:06:37 +0530

    Update Search_an_element_in_a_sorted_and_rotated_Array.cpp
    increased code readibility

--- Search_an_element_in_a_sorted_and_rotated_Array.cpp
+++ Search_an_element_in_a_sorted_and_rotated_Array.cpp
@@ -1,10 +1,8 @@
-// C++ Program to search an element
-// in a sorted and pivoted array
 
 #include <bits/stdc++.h>
 using namespace std;
 
-// Standard Binary Search function
+
 int binarySearch(int arr[], int low, int high, int key)
 {
 	if (high < low)
@@ -20,17 +18,14 @@ int binarySearch(int arr[], int low, int high, int key)
 	return binarySearch(arr, low, (mid - 1), key);
 }
 
-// Function to get pivot. For array 3, 4, 5, 6, 1, 2
-// it returns 3 (index of 6)
+
 int findPivot(int arr[], int low, int high)
 {
-	// Base cases
 	if (high < low)
 		return -1;
 	if (high == low)
 		return low;
 
-	// low + (high - low)/2;
 	int mid = (low + high) / 2;
 	if (mid < high && arr[mid] > arr[mid + 1])
 		return mid;
@@ -44,19 +39,14 @@ int findPivot(int arr[], int low, int high)
 	return findPivot(arr, mid + 1, high);
 }
 
-// Searches an element key in a pivoted
-// sorted array arr[] of size n
+
 int pivotedBinarySearch(int arr[], int n, int key)
 {
 	int pivot = findPivot(arr, 0, n - 1);
 
-	// If we didn't find a pivot,
-	// then array is not rotated at all
 	if (pivot == -1)
 		return binarySearch(arr, 0, n - 1, key);
 
-	// If we found a pivot, then first compare with pivot
-	// and then search in two subarrays around pivot
 	if (arr[pivot] == key)
 		return pivot;
 
@@ -66,7 +56,6 @@ int pivotedBinarySearch(int arr[], int n, int key)
 	return binarySearch(arr, pivot + 1, n - 1, key);
 }
 
-// Driver program to check above functions
 int main()
 {
 	// Let us search 3 in below array
